73637bb3b757262a7a073a4e397e1f37d88c3433
Weverton Marques <wevertonms@gmail.com>
2020-09-03 12:34:12 -0300

    🐛 Ajuste no SECRET_KEY

--- dizimo/settings.py
+++ dizimo/settings.py
@@ -21,7 +21,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY", "c%h%(5a^#n@bd!!f(3@)bt#(cqz&(c0_x4ymy!fv-i!n0zpg@p"
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", True)
