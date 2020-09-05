b2a249c21b3bc602ff1c619f727c4ed65176e4ff
Weverton Marques <wevertonms@gmail.com>
2020-09-05 16:38:43 -0300

    ✨ Permissões automáticas para novos usuários

--- gestao/admin.py
+++ gestao/admin.py
@@ -1,17 +1,91 @@
 import csv
 
 from django.contrib import admin
-from .models import Dizimista, Igreja, Pagamento
-from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import Permission, User
+from django.contrib.contenttypes.models import ContentType
+from django.core.mail import send_mail
+from django.db.models import QuerySet
+from django.http import HttpRequest, HttpResponse
 from django.urls import reverse
 from django.utils.html import format_html
-from django.core.mail import send_mail
-from django.http import HttpResponse
-from django.utils.timezone import now, datetime
+from django.utils.timezone import datetime, now
+from django.utils.translation import gettext_lazy as _
+
+from .models import Dizimista, Igreja, Pagamento, RelatorioPagamentos
 
 admin.site.site_header = "DezPorcento"
 admin.site.site_title = "DezPorcento"
 admin.site.index_title = "Registros"
+
+
+def create_igrejas():
+    Igreja.objects.create(
+        nome="Capela de Nossa Senhora de Quadalupe",
+        endereco="Conjunto Virgem dos Pobres III, s/n",
+    )
+    Igreja.objects.create(
+        nome="Capela de Santa Teresinha",
+        endereco="Praça Tenente Moisés Silva Filho, s/n",
+    )
+    Igreja.objects.create(
+        nome="Igreja de Nossa Senhora do Virgem dos Pobres",
+        endereco="Conjunto Joaquim Leão, s/n",
+    )
+
+
+def get_permission(model, permission: str):
+    model_content_type = ContentType.objects.get_for_model(model)
+    model_permissions = Permission.objects.filter(content_type=model_content_type)
+    return model_permissions.get(codename__startswith=permission)
+
+
+# Unregister the provided model admin
+admin.site.unregister(User)
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = ["date_joined"]
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        if request.user.is_superuser:
+            return qs
+        return qs.filter(username=request.user.username)
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set(["last_login"])
+        if not is_superuser:
+            disabled_fields |= {
+                "username",
+                "is_active",
+                "is_superuser",
+                "is_staff",
+                "groups",
+                "user_permissions",
+            }
+        for field in disabled_fields:
+            if field in form.base_fields:
+                form.base_fields[field].disabled = True
+        return form
+
+    def save_model(self, request, obj: User, form, change):
+        obj.registrado_por = request.user
+        is_new_user = not obj.pk
+        if is_new_user:
+            obj.is_staff = True
+        super().save_model(request, obj, form, change)
+        if is_new_user:
+            new_user_permissions = [
+                get_permission(User, "view"),
+                get_permission(User, "change"),
+                get_permission(Pagamento, "view"),
+                get_permission(Pagamento, "add"),
+            ]
+            obj.user_permissions.set(new_user_permissions)
 
 
 class ExportCsvMixin:
@@ -48,21 +122,25 @@ def filter_month_by_queryset(self, request, queryset, field):
     return queryset
 
 
-class AniversarioMesListFilter(admin.SimpleListFilter):
+class MesListFilter(admin.SimpleListFilter):
     # Human-readable title which will be displayed in the
     # right admin sidebar just above the filter options.
-    title = _("Anversariante do mês")
-
+    title = _("Mês")
     # Parameter for the filter that will be used in the URL query.
-    parameter_name = "aniversario_mes"
+    parameter_name = "mes"
+    field = "data__month"
 
     def lookups(self, request, model_admin):
         return filter_month_by_lookups(self, request, model_admin)
 
     def queryset(self, request, queryset):
-        return filter_month_by_queryset(
-            self, request, queryset, field="nascimento__month"
-        )
+        return filter_month_by_queryset(self, request, queryset, field=self.field)
+
+
+class AniversarioMesListFilter(MesListFilter):
+    title = _("Anversariante do mês")
+    parameter_name = "aniversario_mes"
+    field = "nascimento__month"
 
 
 def endereco(obj):
@@ -87,23 +165,12 @@ class DizimistaAdmin(admin.ModelAdmin, ExportCsvMixin):
 
 @admin.register(Igreja)
 class IgrejaAdmin(admin.ModelAdmin, ExportCsvMixin):
-    list_display = ("nome", "endereco", "número_de_dizimistas")
+    list_display = ("nome", endereco, "número_de_dizimistas")
     search_fields = ["nome"]
     actions = ["export_as_csv"]
 
     def número_de_dizimistas(self, obj):
         return Dizimista.objects.filter(igreja=obj).count()
-
-
-class MesListFilter(admin.SimpleListFilter):
-    title = _("Mês")
-    parameter_name = "mes"
-
-    def lookups(self, request, model_admin):
-        return filter_month_by_lookups(self, request, model_admin)
-
-    def queryset(self, request, queryset):
-        return filter_month_by_queryset(self, request, queryset, field="data__month")
 
 
 @admin.register(Pagamento)
@@ -158,5 +225,10 @@ class PagamentoAdmin(admin.ModelAdmin, ExportCsvMixin):
     def get_queryset(self, request):
         qs = super().get_queryset(request)
         # if not request.user.is_superuser:
-        #     return qs.filter(registrado_por=request.user)
+        # return qs.filter(registrado_por=request.user)
         return qs
+
+
+# @admin.register(RelatorioPagamentos)
+# class RelatorioPagamentosAdmin(admin.ModelAdmin):
+#     pass
