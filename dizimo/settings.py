6edd1a76419dafeff8ea2492e16ab49c4799bcf4
Weverton Marques <wevertonms@gmail.com>
2024-02-07 21:56:14 -0300

    Ajusta configuração ALLOWED_HOSTS

--- dizimo/settings.py
+++ dizimo/settings.py
@@ -41,7 +41,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "dizimo-wms.koyeb.app"]
 
 # Application definition
 
