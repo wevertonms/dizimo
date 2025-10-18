d491f4fbc60719ae43671a6371af771e4fe8661a
Weverton Marques <wevertonm@gmail.com>
2025-10-18 17:24:01 -0300

    Adiciona URL do leapcell a configuração de ALLOWED_HOST

--- dizimo/settings.py
+++ dizimo/settings.py
@@ -41,7 +41,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("SECRET_KEY")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "dizimo-wms.koyeb.app"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "0.0.0.0",
+    "dizimo-wms.koyeb.app",
+    "dizimo-wevertonms5557-lh0l4vqi.leapcell.dev",
+]
 
 # Application definition
 
